a183a3a0c38f91d4a7fa8f47d8a76edee9db73a6
Onur Temiz <onurmain@gmail.com>
2019-05-14 21:25:55 +0300

    Fix Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,16 +1,17 @@
 class TagsController < ApplicationController
-  def show
-    @tag = Tag.find(params[:id])
-  end
-
   def index
     @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
   end
 
   def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
     flash.notice = "Tag '#{@tag.name}' Deleted!"
-    redirect_to tags_path
+
+    redirect_to articles_path
   end
 end
